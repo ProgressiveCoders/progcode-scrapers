fe317bae20322fa4f34a7cf9f3d549d962b640c5
Andy Gimma <andy@hylo.com>
2018-12-13 02:05:25 -0500

    Add some more debug statements

--- app/services/slack_channels_to_airtable.rb
+++ app/services/slack_channels_to_airtable.rb
@@ -1,9 +1,15 @@
 module SlackChannelsToAirtable
   def self.run
+    self.log("Start SlackChannelsToAirtable")
     slack_client = SlackClient.new
     slack_client.channels.each do |channel|
       channel_details = slack_client.channels_info(channel)
       AirtableChannelListTable.add_or_update_channel(channel, channel_details)
     end
+    self.log("Finish SlackChannelsToAirtable")
+  end
+
+  def self.log(msg)
+    puts
   end
 end
--- app/services/slack_members_to_airtable.rb
+++ app/services/slack_members_to_airtable.rb
@@ -1,8 +1,15 @@
 module SlackMembersToAirtable
   def self.run
+    self.log("Start SlackMembersToAirtable")
     slack_client = SlackClient.new
     slack_client.members.each do |member|
       AirtableMembersTable.add_or_update_member(member)
     end
+    self.log("Finish SlackMembersToAirtable")
+
+  end
+
+  def self.log(msg)
+    puts msg
   end
 end
